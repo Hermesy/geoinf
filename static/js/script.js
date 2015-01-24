8f91af79232c50accb0c2bf3f93689ecba50f6e1
Hermes <hermes007@gmail.com>
2015-01-24 14:37:40 +0100

    add connection to firebase

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,15 @@
-angular.module('myApp', [])
-	.controller('myCtrl', function($scope, $http, $page = 1, myFactory){
-		$http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=470fd2ec8853e25d2f8d86f685d2270e&page='+$page).success(function(data) { 
-			$scope.data = data;
-		}); 	
-	})
+angular.module('myApp', ["firebase"])
+	.controller('myCtrl', ["$scope", "$http", "$firebase",  
+		function($scope, $http, $firebase){
+			 
+			 var ref = new Firebase("https://luminous-fire-5765.firebaseio.com/comments");		
+			 var sync = $firebase(ref);
+			 $scope.date = sync.$asObject();
+			 
+			$http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=470fd2ec8853e25d2f8d86f685d2270e&page=1').success(function(data) { 
+				$scope.data = data;
+			}); 	
+	}])
 	.factory('myFactory', function() {
 		var myFactoryService;
 
