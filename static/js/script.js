ed8779c9cf013bb98b705e41f2da4f46e8474f11
Hermes <hermes007@gmail.com>
2015-01-25 00:21:46 +0100

    create mechanism for adding comments, move loading data functionality to factory

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,53 @@
 angular.module('myApp', ["firebase"])
-	.controller('myCtrl', ["$scope", "$http", "$firebase",  
-		function($scope, $http, $firebase){
-			 
-			 var ref = new Firebase("https://luminous-fire-5765.firebaseio.com/comments");		
-			 var sync = $firebase(ref);
-			 $scope.date = sync.$asObject();
-			 
-			$http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=470fd2ec8853e25d2f8d86f685d2270e&page=1').success(function(data) { 
-				$scope.data = data;
-			}); 	
-	}])
-	.factory('myFactory', function() {
-		var myFactoryService;
+	.factory('myFactory', function($http, $firebase, $q) {
+		var ref = new Firebase("https://luminous-fire-5765.firebaseio.com/comments/");		
+		var sync = $firebase(ref);
+		var items = sync.$asArray();
+		
+		var getData = function() {
+			var promise = $http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=470fd2ec8853e25d2f8d86f685d2270e&page=1')
+				.success(function(response) { 
+					return response;
+				});
+			return promise;
+		}
+		
+		var getComments = function() {
+			return items;
+		}
+		
+		var addComment = function(object) {
+			items.$add(object);
+		}
 
-		return myFactoryService;
-  });
+		return {
+			getData: getData,
+			getComments: getComments,
+			addComment: addComment
+		}
+	})
+	.controller('myCtrl', function($scope, $http, $firebase, myFactory){
+			 
+		myFactory.getData().success(function(d){
+			$scope.data = d;
+			$scope.initialCommentModel();
+		});
+		
+		$scope.addComment = function() {
+			myFactory.addComment({id:$scope.comment.id, author:$scope.comment.author, text:$scope.comment.text});
+			$('#myModal').modal('hide');
+			$scope.initialCommentModel();
+		}
+		
+		$scope.fillIdInModal = function(id) {
+			$scope.comment.id = id;	
+		}
+		
+		$scope.initialCommentModel = function() {
+			$scope.comment = {};
+			$scope.comment.id = "";
+			$scope.comment.author = "";
+			$scope.comment.text = "";
+		}		
+
+	});
