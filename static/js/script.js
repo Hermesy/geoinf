09a7f6b33ab0438bc883dfb12298a44e6619836d
Hermes <hermes007@gmail.com>
2015-01-25 23:27:55 +0100

    update mechanism for authentication, add delete comments function

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 angular.module('myApp', ["firebase"])
 	.controller('myCtrl', function($scope, $http, $firebase, myFactory){
+		$scope.auth = false;
 			 
 		myFactory.getData().success(function(d){
 			$scope.data = d;
@@ -7,38 +8,50 @@ angular.module('myApp', ["firebase"])
 		});
 		
 		$scope.comments = myFactory.getComments();
-		
-		var ref2 = new Firebase("https://luminous-fire-5765.firebaseio.com/");
-		ref2.authWithPassword({
-			email : "hermes007@gmail.com",
-			password : "dupasmoka"
-		}, function(error, authData) {
-			if (error)
-				$scope.auth = false;
-			else
-				$scope.auth = true
+				
+		$scope.logIn = function() {
+			var ref2 = new Firebase("https://luminous-fire-5765.firebaseio.com/");
+			ref2.authWithPassword({
+				email : $scope.user.email,
+				password : $scope.user.password
+			}, function(error, authData) {
+				if (error) {
+					alert(error);
+					$scope.auth = false;
+				} else {
+					alert("Authorization correct");
+					$('#myModal2').modal('hide');
+					$scope.auth = true;
+				}
 			});	
+		};
 		
+		$scope.logOut = function() {	
+			var ref2 = new Firebase("https://luminous-fire-5765.firebaseio.com/");
+			ref2.unauth();
+			$scope.auth = false;
+		};
+					
 		$scope.addComment = function() {
 			myFactory.addComment({id:$scope.comment.id, author:$scope.comment.author, text:$scope.comment.text});
 			$('#myModal').modal('hide');
 			$scope.initialCommentModel();
-		}
+		};
 		
 		$scope.fillIdInModal = function(id) {
 			$scope.comment.id = id;	
-		}
+		};
 		
 		$scope.initialCommentModel = function() {
 			$scope.comment = {};
 			$scope.comment.id = "";
 			$scope.comment.author = "";
 			$scope.comment.text = "";
-		}	
+		};	
 
 		$scope.deleteComment = function(id) {
 			myFactory.deleteComment(id);
-		}
+		};
 
 	})
 	.factory('myFactory', function($http, $firebase, $q, $firebaseAuth) {
