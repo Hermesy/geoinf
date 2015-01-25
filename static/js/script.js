ad9ff31d167d8546f9288b94823e22a0aac4fd31
Hermes <hermes007@gmail.com>
2015-01-25 15:46:26 +0100

    add authentication and mechanism for deleting comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,48 @@
 angular.module('myApp', ["firebase"])
-	.factory('myFactory', function($http, $firebase, $q) {
-		var ref = new Firebase("https://luminous-fire-5765.firebaseio.com/comments/");		
+	.controller('myCtrl', function($scope, $http, $firebase, myFactory){
+			 
+		myFactory.getData().success(function(d){
+			$scope.data = d;
+			$scope.initialCommentModel();
+		});
+		
+		$scope.comments = myFactory.getComments();
+		
+		var ref2 = new Firebase("https://luminous-fire-5765.firebaseio.com/");
+		ref2.authWithPassword({
+			email : "hermes007@gmail.com",
+			password : "dupasmoka"
+		}, function(error, authData) {
+			if (error)
+				$scope.auth = false;
+			else
+				$scope.auth = true
+			});	
+		
+		$scope.addComment = function() {
+			myFactory.addComment({id:$scope.comment.id, author:$scope.comment.author, text:$scope.comment.text});
+			$('#myModal').modal('hide');
+			$scope.initialCommentModel();
+		}
+		
+		$scope.fillIdInModal = function(id) {
+			$scope.comment.id = id;	
+		}
+		
+		$scope.initialCommentModel = function() {
+			$scope.comment = {};
+			$scope.comment.id = "";
+			$scope.comment.author = "";
+			$scope.comment.text = "";
+		}	
+
+		$scope.deleteComment = function(id) {
+			myFactory.deleteComment(id);
+		}
+
+	})
+	.factory('myFactory', function($http, $firebase, $q, $firebaseAuth) {
+		var ref = new Firebase("https://luminous-fire-5765.firebaseio.com/comments/");	
 		var sync = $firebase(ref);
 		var items = sync.$asArray();
 		
@@ -19,37 +61,16 @@ angular.module('myApp', ["firebase"])
 		var addComment = function(object) {
 			items.$add(object);
 		}
+		
+		var deleteComment = function(id) {
+			var fredRef = new Firebase("https://luminous-fire-5765.firebaseio.com/comments/"+id);
+			fredRef.remove();
+		}
 
 		return {
 			getData: getData,
 			getComments: getComments,
-			addComment: addComment
+			addComment: addComment,
+			deleteComment: deleteComment
 		}
-	})
-	.controller('myCtrl', function($scope, $http, $firebase, myFactory){
-			 
-		myFactory.getData().success(function(d){
-			$scope.data = d;
-			$scope.initialCommentModel();
-		});
-		
-		$scope.comments = myFactory.getComments();
-		
-		$scope.addComment = function() {
-			myFactory.addComment({id:$scope.comment.id, author:$scope.comment.author, text:$scope.comment.text});
-			$('#myModal').modal('hide');
-			$scope.initialCommentModel();
-		}
-		
-		$scope.fillIdInModal = function(id) {
-			$scope.comment.id = id;	
-		}
-		
-		$scope.initialCommentModel = function() {
-			$scope.comment = {};
-			$scope.comment.id = "";
-			$scope.comment.author = "";
-			$scope.comment.text = "";
-		}		
-
 	});
